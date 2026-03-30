4d12063eb056a79e7d5715ad669f84f8dcbe3b16
itserichuang <141202406+itserichuang@users.noreply.github.com>
2026-03-30 12:06:32 -0400

    updated shooter offset, limelight offset, and intake pivot gear ratio

--- src/main/java/frc/robot/subsystems/intakepivot/IntakePivotConstants.java
+++ src/main/java/frc/robot/subsystems/intakepivot/IntakePivotConstants.java
@@ -37,7 +37,7 @@ public class IntakePivotConstants {
   public static final int stallCurrentLimit = 38;
   public static final int freeCurrentLimit = 38;
   public static final int supplyCurrentLimit = 38;
-  public static final double motorReduction = 94.5 / 1.0;
+  public static final double motorReduction = 210.0 / 1.0; // 94.5 / 1.0;
   public static final boolean isInverted = false;
   public static final double encoderPositionFactor = 2 * Math.PI; // Rotations -> Radians
   public static final double encoderVelocityFactor = (2 * Math.PI) / 60.0; // RPM -> Rad/Sec
